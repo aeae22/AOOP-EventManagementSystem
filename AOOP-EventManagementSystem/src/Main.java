2d20bac156f54b4ad26af1b5821ae3374fecfd54
Joaquín V <jvillonpecho220107@gmail.com>
2026-03-19 18:19:26 -0400

    Add main to test the progarm

--- AOOP-EventManagementSystem/src/Main.java
+++ AOOP-EventManagementSystem/src/Main.java
@@ -1,13 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Event event = new Event("Tech Conference", "2026-04-10", 100.0);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        TicketType regular = new TicketType("Regular", 1.0);
+        TicketType vip = new TicketType("VIP", 1.5);
+
+        event.addTicketType(regular);
+        event.addTicketType(vip);
+
+        Attendee attendee = new Attendee(
+                "Alice Johnson",
+                "alice@example.com",
+                "123 Main Street"
+        );
+
+        TicketCart cart = new TicketCart();
+        cart.addTicket(event, regular, 2);
+        cart.addTicket(event, vip, 1);
+
+        System.out.println(event);
+        System.out.println(attendee);
+        System.out.println(cart);
+
+        EventOrder order = new EventOrder(attendee, "2026-03-19");
+
+        for (CartItem item : cart.getItems()) {
+            Ticket ticket = new Ticket(
+                    item.getEvent(),
+                    item.getTicketType(),
+                    item.getQuantity()
+            );
+            order.addTicket(ticket);
+        }
+
+        order.calculateTotal();
+
+        System.out.println(order);
+
+        for (Ticket ticket : order.getTickets()) {
+            System.out.println(ticket);
+        }
+
+        System.out.println("Total events created: " + Event.getTotalEvents());
     }
 }
