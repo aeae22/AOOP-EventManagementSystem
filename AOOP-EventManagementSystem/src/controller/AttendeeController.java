9ac0dca74cf061b17625f7b1ba5fd068d04b5154
Joaquín V <jvillonpecho220107@gmail.com>
2026-03-31 16:18:46 -0400

    Update model classes and main

--- AOOP-EventManagementSystem/src/controller/AttendeeController.java
+++ AOOP-EventManagementSystem/src/controller/AttendeeController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import model.Attendee;
+import java.util.ArrayList;
+
 public class AttendeeController {
+    private ArrayList<Attendee> attendees;
+
+    public AttendeeController() {
+        attendees = new ArrayList<>();
+    }
+
+    public void addAttendee(Attendee attendee) {
+        attendees.add(attendee);
+    }
+
+    public ArrayList<Attendee> getAttendees() {
+        return attendees;
+    }
+
+    public Attendee findAttendeeByEmail(String email) {
+        for (Attendee attendee : attendees) {
+            if (attendee.getEmail().equalsIgnoreCase(email)) {
+                return attendee;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeAttendee(String email) {
+        Attendee attendee = findAttendeeByEmail(email);
+        if (attendee != null) {
+            attendees.remove(attendee);
+            return true;
+        }
+        return false;
+    }
 }
