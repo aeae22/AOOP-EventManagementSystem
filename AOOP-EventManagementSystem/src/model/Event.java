2d00523fe628ae22289158f7aa03eec5562bda83
kevalraval2011 <kevalraval2010@gmail.com>
2026-04-10 11:34:28 -0400

    Update Event.java

--- AOOP-EventManagementSystem/src/model/Event.java
+++ AOOP-EventManagementSystem/src/model/Event.java
@@ -1,87 +1,15 @@
 package model;
 
-import java.util.ArrayList;
+import jakarta.persistence.*;
+import java.util.List;
 
+@Entity 
 public class Event {
-    private String title;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
     private String date;
-    private double basePrice;
-    private ArrayList<TicketType> ticketTypes;
-    private static int totalEvents = 0;
-    private int capacity;
-    private ArrayList<Attendee> attendees;
 
-    public Event(String title, String date, double basePrice, int capacity) {
-        this.title = title;
-        this.date = date;
-        this.basePrice = basePrice;
-        this.capacity = capacity;
-        this.ticketTypes = new ArrayList<>();
-        this.attendees = new ArrayList<>();
-        totalEvents++;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public double getBasePrice() {
-        return basePrice;
-    }
-
-    public void setBasePrice(double basePrice) {
-        this.basePrice = basePrice;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public ArrayList<Attendee> getAttendees() {
-        return attendees;
-    }
-
-    public void addAttendee(Attendee attendee) {
-        attendees.add(attendee);
-    }
-
-    public void addTicketType(TicketType type) {
-        ticketTypes.add(type);
-    }
-
-    public ArrayList<TicketType> getTicketTypes() {
-        return ticketTypes;
-    }
-
-    public static int getTotalEvents() {
-        return totalEvents;
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "title='" + title + '\'' +
-                ", date='" + date + '\'' +
-                ", basePrice=" + basePrice +
-                ", capacity=" + capacity +
-                ", attendees=" + attendees.size() +
-                ", ticketTypes=" + ticketTypes.size() +
-                '}';
-    }
+    
 }
